b530e784f756f068f236acbb83d79c78eb13e053
ebrucihan <ebrchnnn123@gmail.com>
2024-10-20 14:37:02 +0300

    addition

--- bill-gates/src/App.jsx
+++ bill-gates/src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./components/Header.jsx";
 import ProductList from "./components/ProductList.jsx";
 
 import "./index.css";
-import "./App.css";
 
 function App() {
   return (
--- bill-gates/src/main.jsx
+++ bill-gates/src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./css/header.css";
+import "./css/money.css";
+import "./css/productlist.css";
+import "./css/receipt.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
