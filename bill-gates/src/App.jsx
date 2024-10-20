658324d9baac088aa454bd99098f3a2f866a14db
ebrucihan <ebrchnnn123@gmail.com>
2024-10-20 14:44:41 +0300

    addition

--- bill-gates/src/App.jsx
+++ bill-gates/src/App.jsx
@@ -3,6 +3,10 @@ import Header from "./components/Header.jsx";
 import ProductList from "./components/ProductList.jsx";
 
 import "./index.css";
+import "./css/header.css";
+import "./css/money.css";
+import "./css/productlist.css";
+import "./css/receipt.css";
 
 function App() {
   return (
